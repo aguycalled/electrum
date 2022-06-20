76f9f568ea1828c6b17ca42a0405c9721e208e5a
alex <alex@nav.community>
2022-06-20 15:02:11 +0200

    bump navhip

--- electrum/version.py
+++ electrum/version.py
@@ -1,5 +1,5 @@
-ELECTRUM_VERSION = '5.2.10' # version of the client package
-APK_VERSION = '5.2.9'      # read by buildozer.spec
+ELECTRUM_VERSION = '5.2.11' # version of the client package
+APK_VERSION = '5.2.11'      # read by buildozer.spec
 
 PROTOCOL_VERSION = '1.5'   # protocol version requested
 
